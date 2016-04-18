e3b83d65e4289d12002b427c54c9403c13d10316
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-19 00:29:04 +0200

    license headers

--- src/main/java/com/graphaware/neo4j/graphgen/generator/GraphGenerator.java
+++ src/main/java/com/graphaware/neo4j/graphgen/generator/GraphGenerator.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2013-2016 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.graphaware.neo4j.graphgen.generator;
 
 import com.graphaware.neo4j.graphgen.YamlParser;
-import com.graphaware.neo4j.graphgen.configuration.GraphgenConfiguration;
 import com.graphaware.neo4j.graphgen.faker.FakerService;
 import com.graphaware.neo4j.graphgen.graph.Property;
 import com.graphaware.neo4j.graphgen.util.CountSyntaxUtil;
@@ -9,7 +24,6 @@ import com.graphaware.neo4j.graphgen.util.ShuffleUtil;
 import org.neo4j.graphdb.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
